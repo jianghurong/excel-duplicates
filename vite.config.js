7192d9a2759b02fa60ed23a7481f5f3f078b537a
jianghurong <jianghr@mobduos.com>
2023-11-09 12:23:22 +0800

    feat: 更新

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/excel-duplicates/',
   plugins: [
     vue(),
     Components({
